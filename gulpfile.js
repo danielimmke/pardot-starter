29fee9a6fa64fac5d4282af15ef4440516e64af7
Daniel Immke <me@daniel.do>
2019-10-07 16:24:10 -0400

    Refactor for Gulp 4, revise documentation, improve support for JS and CSS.

--- gulpfile.js
+++ gulpfile.js
@@ -1,90 +1,87 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var notify = require('gulp-notify');
-var minifycss = require('gulp-clean-css');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var inline = require('gulp-inline');
-var rename = require('gulp-rename');
-var entities = require('gulp-html-entities');
-var nunjucksRender = require('gulp-nunjucks-render');
+/**
+* Imports
+*/
+const { src, dest, series, watch } = require( 'gulp' ),
+	sass			= require( 'gulp-sass' ),
+	notify			= require( 'gulp-notify' ),
+	minifycss		= require( 'gulp-clean-css' ),
+	concat			= require( 'gulp-concat' ),
+	uglify			= require( 'gulp-uglify' ),
+	inline			= require( 'gulp-inline' ),
+	entities		= require( 'gulp-html-entities' ),
+	nunjucksRender	= require( 'gulp-nunjucks-render' );
 
-// Style Paths
-var scss = [
-	'assets/scss/*'
-]
-
-// JS Paths
-var js = [
-	'assets/js/*'
-];
-
-// Source Files
-var source = [
-	'source/**/*'
-];
+/**
+* Paths
+*/
+const paths = {
+	scss: 	[ 'assets/scss/*' ],
+	js: 	[ 'assets/js/*' ],
+	source: [ 'source/**/*' ]
+}
 
 /**
 * Process SASS and output to style.css at parent directory.
 */
-gulp.task('sass', function(){
-	return gulp.src(scss)
-		.pipe(sass({style: 'compact'}))
-		.pipe(minifycss({keepBreaks: false}))
-		.pipe(gulp.dest('build/'))
-		.pipe(notify('SASS done.'));
-});
+const sassJob = function () {
+	return src( paths.scss)
+		.pipe( sass( {style: 'compact'}) )
+		.pipe( minifycss( {keepBreaks: false}) )
+		.pipe( dest( 'build/' ) )
+		.pipe( notify( 'SASS done.' ) );
+}
 
 /**
 * Concatenate scripts from /js/ folder, minify them and output to scripts.js at parent directory.
 */
-gulp.task('js', function(){
-	return gulp.src(js)
-		.pipe(concat('scripts.js'))
-		.pipe(gulp.dest('build/'))
-		.pipe(uglify())
-		.pipe(gulp.dest('build/'))
-		.pipe(notify('JS done.'));
-});
+const jsJob = function () {
+	return src( paths.js )
+		.pipe( concat( 'scripts.js' ) )
+		.pipe( dest( 'build/' ) )
+		.pipe( uglify() )
+		.pipe( dest( 'build/' ) )
+		.pipe( notify( 'JS done.' ) );
+}
 
 /**
 * Pull partials into templates and compile into /preview/ folder.
 */
-gulp.task('nunjucks', function() {
-  return gulp.src('source/*.+(html|nunjucks)')
-  .pipe(nunjucksRender({
-      path: ['source/']
-    }))
-  .pipe(gulp.dest('preview/'))
-});
+const nunjucksJob = function () {
+  return src( 'source/*.+(html|nunjucks)' )
+  .pipe( nunjucksRender( {
+      path: [ 'source/' ]
+    }) )
+  .pipe( dest( 'preview/' ) )
+};
 
 /**
 * Take linked CSS/JS files in html files in /preview/ and inline them, printing everything to /build/ folder.
 */
-gulp.task('build', function(){
-	return gulp.src('preview/*.html')
-	.pipe(inline({
+const buildJob = function () {
+	return src( 'preview/*.html' )
+	.pipe( inline( {
 		base: './',
-	}))
-	.pipe(entities('encodeNonASCII'))
-	.pipe(gulp.dest('build/'))
-	.pipe(notify('Inserted styles and scripts inline into index.html.'));
-});
+	}) )
+	.pipe( entities( 'encodeNonASCII' ) )
+	.pipe( dest( 'build/' ) )
+	.pipe( notify( 'Inserted styles and scripts inline into index.html.' ) );
+};
 
 /**
 * Watch
 */
-gulp.task('watch', function(){
-	gulp.watch(scss, ['sass']);
-	gulp.watch(js, ['js']);
-	gulp.watch(source, ['nunjucks']);
-});
+const watchJob = function () {
+	watch( paths.scss, sassJob ),
+	watch( paths.js, jsJob ),
+	watch( paths.source, nunjucksJob )
+};
 
 /**
-* Default
+* Export Tasks
 */
-gulp.task('default', ['sass', 'js', 'watch']);
-
-//TODO: Font woff inline -> @font-family in CSS
-
-//TODO: Batch image upload to Pardot
+exports.sass = sassJob;
+exports.js = jsJob;
+exports.nunjucks = nunjucksJob;
+exports.watch = watchJob;
+exports.build = buildJob;
+exports.default = series( sassJob, jsJob, watchJob );
